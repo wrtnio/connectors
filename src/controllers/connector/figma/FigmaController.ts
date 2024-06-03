3c5e3c60541bd02eaf5c8d52609425e16da58b95
kakasoo <kscodebase@gmail.com>
2024-06-03 17:50:47 +0900

    feat: Defined the figma read file API and its DTO metadata
    - Install the @figma/rest-api-spec library for defining DTO metadata.

--- src/controllers/connector/figma/FigmaController.ts
+++ src/controllers/connector/figma/FigmaController.ts
@@ -1,8 +1,28 @@
+import core from "@nestia/core";
 import { Controller } from "@nestjs/common";
+
+import { IFigma } from "@wrtn/connector-api/lib/structures/connector/figma/IFigma";
 
 import { FigmaProvider } from "../../../providers/figma/FigmaProvider";
 
 @Controller("connector/figma")
 export class FigmaController {
   constructor(private readonly figmaProvider: FigmaProvider) {}
+
+  /**
+   * 피그마 파일들을 가져옵니다.
+   *
+   * @summary 피그마 파일 가져오기.
+   *
+   * @returns 피그마 파일 목록
+   *
+   * @param input
+   * @returns
+   */
+  @core.TypedRoute.Post("get-files")
+  async readFiles(
+    @core.TypedBody() input: IFigma.IReadFileInput,
+  ): Promise<IFigma.IReadFileOutput> {
+    return null!;
+  }
 }
--- src/providers/figma/FigmaProvider.ts
+++ src/providers/figma/FigmaProvider.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
+import { IFigma } from "@wrtn/connector-api/lib/structures/connector/figma/IFigma";
+
 @Injectable()
-export class FigmaProvider {}
+export class FigmaProvider {
+  async getFiles(
+    input: IFigma.IReadFileInput,
+  ): Promise<IFigma.IReadFileOutput> {
+    return null!;
+  }
+}
