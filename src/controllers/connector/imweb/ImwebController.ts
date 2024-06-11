a0bc5c126874a4d8a8b899dd2813cf03046cc216
kakasoo <kscodebase@gmail.com>
2024-06-11 21:51:43 +0900

    feat: add Provider of getProducts in `imweb` #31

--- src/controllers/connector/imweb/ImwebController.ts
+++ src/controllers/connector/imweb/ImwebController.ts
@@ -7,10 +7,19 @@ import { ImwebProvider } from "../../../providers/connector/imweb/ImwebProvider"
 
 @Controller("connector/imweb")
 export class ImwebController {
+  /**
+   * 아임웹에서 판매자의 상품들을 불러온다.
+   *
+   * @summary 자신의 상품 조회.
+   *
+   * @param input 상품을 조회하기 위한 조건 DTO.
+   * @returns 액세스 토큰을 담은 응답 DTO.
+   */
+  @core.TypedRoute.Post("get-products")
   async getProducts(
     @TypedBody() input: IImweb.IGetProductInput,
   ): Promise<IImweb.IGetProductOutput> {
-    return null!;
+    return ImwebProvider.getProducts(input);
   }
 
   /**
