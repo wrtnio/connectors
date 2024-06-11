651540d494e31f6a87e311b34dec7ec2233f0ef4
kakasoo <kscodebase@gmail.com>
2024-06-11 15:28:12 +0900

    feat: get access token of imweb api #30

--- src/controllers/connector/imweb/ImwebController.ts
+++ src/controllers/connector/imweb/ImwebController.ts
@@ -3,12 +3,22 @@ import { Controller } from "@nestjs/common";
 
 import { IImweb } from "@wrtn/connector-api/lib/structures/connector/imweb/IImweb";
 
+import { ImwebProvider } from "../../../providers/connector/imweb/ImwebProvider";
+
 @Controller("connector/imweb")
 export class ImwebController {
-  @core.TypedRoute.Get("auth")
+  /**
+   * 아임웹 액세스 토큰 발급.
+   *
+   * @internal
+   *
+   * @param input 액세스 토큰 발급을 위한 요청 DTO.
+   * @returns 액세스 토큰을 담은 응답 DTO.
+   */
+  @core.TypedRoute.Post("auth")
   async authorization(
     @TypedBody() input: IImweb.Credential,
   ): Promise<IImweb.IGetAccessTokenOutput> {
-    return null!;
+    return ImwebProvider.getAccessToken(input);
   }
 }
