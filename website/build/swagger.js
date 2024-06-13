18d40f760c657f6e53197c42ca3e87a3203338b0
Jeongho Nam <samchon.github@gmail.com>
2024-06-13 15:37:02 +0900

    Prepare website deployment

---
+++ website/build/swagger.js
@@ -0,0 +1,14 @@
+const cp = require("child_process");
+const fs = require("fs");
+
+const main = () => {
+  cp.execSync("npm run build:swagger", {
+    stdio: "inherit",
+    cwd: `${__dirname}/../..`,
+  });
+  fs.copyFileSync(
+    `${__dirname}/../packages/api/swagger.json`,
+    `${__dirname}/out/swagger/swagger.json`,
+  );
+};
+main();
