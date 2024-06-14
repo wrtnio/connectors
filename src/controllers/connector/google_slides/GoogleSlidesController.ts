538772a122321d96d0a7ae365cbea646b3b75161
kakasoo <kscodebase@gmail.com>
2024-06-14 15:29:15 +0900

    docs: 구글 슬라이드 프레젠테이션 생성에 대한 주석 추가 #56

--- src/controllers/connector/google_slides/GoogleSlidesController.ts
+++ src/controllers/connector/google_slides/GoogleSlidesController.ts
@@ -12,6 +12,14 @@ export class GoogleSlidesController {
   @core.TypedRoute.Post("get-presentations")
   async getPresentation() {}
 
+  /**
+   * Google Slides 프레젠테이션을 생성합니다.
+   *
+   * @summary Google Slides 프레젠테이션 생성.
+   *
+   * @param input 프레젠테이션을 만들기 위한 조건 DTO.
+   * @returns 생성된 프레젠테이션 정보 DTO.
+   */
   @core.TypedRoute.Post("presentations")
   async createPresentation(
     @TypedBody() input: IGoogleSlides.ICreatePresentationInput,
