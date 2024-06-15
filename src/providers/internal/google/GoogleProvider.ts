0616d9a97f30dbbf6ee7249260dd8885acafc077
kakasoo <kscodebase@gmail.com>
2024-06-15 14:22:02 +0900

    chore: GoogleProvider를 의존성 주입 없이 사용하고자 namespace로 재정의

--- src/providers/internal/google/GoogleProvider.ts
+++ src/providers/internal/google/GoogleProvider.ts
@@ -9,8 +9,6 @@ export class GoogleProvider {
   private readonly googleClientSecret =
     ConnectorGlobal.env.GOOGLE_CLIENT_SECRET;
 
-  constructor() {}
-
   async refreshAccessToken(secretKey: string): Promise<string> {
     const client = new google.auth.OAuth2(
       this.googleClientId,
@@ -51,5 +49,30 @@ export namespace GoogleProvider {
         },
       ];
     };
+  }
+}
+
+export namespace GoogleProvider {
+  export const googleClientId = ConnectorGlobal.env.GOOGLE_CLIENT_ID;
+  export const googleClientSecret = ConnectorGlobal.env.GOOGLE_CLIENT_SECRET;
+
+  export async function refreshAccessToken(secretKey: string): Promise<string> {
+    const client = new google.auth.OAuth2(
+      GoogleProvider.googleClientId,
+      GoogleProvider.googleClientSecret,
+    );
+
+    client.setCredentials({
+      refresh_token: decodeURIComponent(secretKey),
+    });
+
+    const { credentials } = await client.refreshAccessToken();
+    const accessToken = credentials.access_token;
+
+    if (!accessToken) {
+      throw new InternalServerErrorException("Failed to refresh access token");
+    }
+
+    return accessToken;
   }
 }
