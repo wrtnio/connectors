75ff7ee92e04d4520f603ce9d1192235d56b4a1a
kakasoo <kscodebase@gmail.com>
2024-06-19 16:03:25 +0900

    docs: Added comments of `getShortTermForecast` connector #72

--- src/controllers/connector/open_data/OpenDataController.ts
+++ src/controllers/connector/open_data/OpenDataController.ts
@@ -1,5 +1,6 @@
 import { TypedBody, TypedRoute } from "@nestia/core";
 import { Controller } from "@nestjs/common";
+import { Standalone } from "@wrtn/decorators";
 
 import { IOpenData } from "@wrtn/connector-api/lib/structures/connector/open_data/IOpenData";
 
@@ -7,6 +8,15 @@ import { OpenDataProvider } from "../../../providers/connector/open_data/OpenDat
 
 @Controller("connector/open-data")
 export class OpenDataController {
+  /**
+   * 오늘 날씨를 조회합니다.
+   *
+   * @summary 기상청 오늘 날씨 조회.
+   *
+   * @param input 날씨 조회를 위한 위치 정보 DTO.
+   * @returns 해당 지역의 기상 정보.
+   */
+  @Standalone()
   @TypedRoute.Post("getShortTermForecast")
   async getShortTermForecast(
     @TypedBody()
