9d6ddb95da2bde28e1eb133101e345089862276c
kakasoo <kscodebase@gmail.com>
2024-06-22 16:53:20 +0900

    feat: add provider logic for getting job openings in work24 #95

--- src/providers/connector/work24/Work24Provider.ts
+++ src/providers/connector/work24/Work24Provider.ts
@@ -1,1 +1,24 @@
-export namespace Work24Provider {}
+import axios from "axios";
+
+import { IWork24 } from "@wrtn/connector-api/lib/structures/connector/work24/IWork24";
+
+import { ConnectorGlobal } from "../../../ConnectorGlobal";
+import { ObjectToQueryString } from "../../../utils/ObjectToQueryString";
+import { convertXmlToJson } from "../../../utils/convertXmlToJson";
+
+export namespace Work24Provider {
+  export async function getJobOpenings(input: IWork24.IGetJobOpeningInput) {
+    const { careerOptions, salaryOptions, ...rest } = input;
+
+    const baseUrl = `https://www.work24.go.kr/cm/openApi/call/wk/callOpenApiSvcInfo210L01.do`;
+
+    const q1 = ObjectToQueryString(careerOptions ?? {});
+    const q2 = ObjectToQueryString(salaryOptions ?? {});
+    const q3 = ObjectToQueryString(rest);
+    const authKey = ConnectorGlobal.env.WORK24_AUTH_KEY;
+    const url = `${baseUrl}?${q1}&${q2}&${q3}&authKey=${authKey}&callTp=L`;
+    const res = await axios.get(url);
+
+    return convertXmlToJson(res.data);
+  }
+}
