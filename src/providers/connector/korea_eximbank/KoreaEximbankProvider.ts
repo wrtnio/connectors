1be4217afa4f169bee1eab4ca2e0385a78a150ea
kakasoo <kscodebase@gmail.com>
2024-06-24 13:14:52 +0900

    fix: modify endpoint's scheme https to http #91

--- src/providers/connector/korea_eximbank/KoreaEximbankProvider.ts
+++ src/providers/connector/korea_eximbank/KoreaEximbankProvider.ts
@@ -4,16 +4,22 @@ import { ConnectorGlobal } from "../../../ConnectorGlobal";
 
 export namespace KoreaEximbankProvider {
   export async function getExchange() {
-    const baseUrl = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON`;
-    const authKey = ConnectorGlobal.env.KOREA_EXIM_BANK_API_KEY;
+    try {
+      const baseUrl = `http://www.koreaexim.go.kr/site/program/financial/exchangeJSON`;
+      const authKey = ConnectorGlobal.env.KOREA_EXIM_BANK_API_KEY;
 
-    const url = `${baseUrl}?authkey=${authKey}&data=AP01`;
+      const url = `${baseUrl}?authkey=${authKey}&data=AP01`;
 
-    const res = await axios.get(url, {
-      headers: {
-        "User-Agent": "PostmanRuntime/7.39.0",
-      },
-    });
-    return res.data;
+      const res = await axios.get(url, {
+        headers: {
+          "User-Agent": "PostmanRuntime/7.39.0",
+        },
+      });
+
+      return res.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 }
