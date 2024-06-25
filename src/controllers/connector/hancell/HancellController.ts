6fa09fa1aa5b89d446596892f231f2b45986e887
kakasoo <kscodebase@gmail.com>
2024-06-26 08:35:24 +0900

    chore: hancell에 ApiTags 추가#26

--- src/controllers/connector/hancell/HancellController.ts
+++ src/controllers/connector/hancell/HancellController.ts
@@ -1,5 +1,6 @@
 import core, { TypedBody } from "@nestia/core";
 import { Controller } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { Standalone } from "@wrtn/decorators";
 
 import { IHancell } from "@wrtn/connector-api/lib/structures/connector/hancell/IHancell";
@@ -15,6 +16,60 @@ export class HancellController {
    * @param input 읽을 한셀 파일 정보
    * @returns 한셀 파일 정보
    */
+  @ApiTags(
+    "Hancell 엑셀 파일",
+    "엑셀",
+    "파일",
+    "내보내기",
+    "다운로드",
+    "추출",
+    "추출하기",
+    "스프레드시트",
+    "데이터 저장",
+    "데이터 불러오기",
+    "데이터 분석",
+    "스프레드시트",
+    "데이터 베이스",
+    "데이터 내보내기",
+    "데이터 가져오기",
+    "엑셀 변환",
+    "텍스트 파일",
+    "데이터 처리",
+    "대량 데이터",
+    "데이터 편집",
+    "파일 분할",
+    "데이터 통합",
+    "엑셀 만들기",
+    "엑셀 파일 열기",
+    "데이터 추출",
+    "데이터 필터링",
+    "데이터 병합",
+    "Hancell",
+    "File",
+    "Export",
+    "Download",
+    "Extract",
+    "Spreadsheet",
+    "Save Data",
+    "Load Data",
+    "Data Analysis",
+    "Spreadsheet",
+    "Database",
+    "Export Data",
+    "Import Data",
+    "Convert to Hancell",
+    "Text File",
+    "Data Processing",
+    "Large Data",
+    "Edit Data",
+    "Split File",
+    "Integrate Data",
+    "Create Hancell",
+    "Open Hancell File",
+    "Extract Data",
+    "Filter Data",
+    "Merge Data",
+  )
   @Standalone()
   @core.TypedRoute.Post("read")
   async read(
