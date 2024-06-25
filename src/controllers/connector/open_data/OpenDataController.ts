80afd39933c396859d5eb828fa7e5b3f4233a053
kakasoo <kscodebase@gmail.com>
2024-06-25 15:31:53 +0900

    feat: 주차장 정보 조회에 대한 주석 추가 #119

--- src/controllers/connector/open_data/OpenDataController.ts
+++ src/controllers/connector/open_data/OpenDataController.ts
@@ -10,6 +10,13 @@ import { OpenDataProvider } from "../../../providers/connector/open_data/OpenDat
 
 @Controller("connector/open-data")
 export class OpenDataController {
+  /**
+   * [한국지능정보사회진흥원] 주차장 정보를 조회합니다.
+   *
+   * @summary 주차장 조회
+   * @param input 주차장 조회 조건
+   * @returns 주차장 정보
+   */
   @TypedRoute.Post("getParkingLot")
   async getParkingLot(
     @TypedBody() input: INIA.IGetParkingLotInput,
@@ -18,7 +25,7 @@ export class OpenDataController {
   }
 
   /**
-   * 국토교통부 건축물대장정보를 조회합니다.
+   * [국토교통부] 건축물대장정보를 조회합니다.
    *
    * @summary 건축물대장정보 조회
    * @param input 빌딩 정보를 조회하는 조건
@@ -32,7 +39,7 @@ export class OpenDataController {
   }
 
   /**
-   * 국내 지역의 행정 표준 상 코드를 조회합니다.
+   * [행정안전부] 국내 지역의 행정 표준 상 코드를 조회합니다.
    *
    * @summary 행정표준 코드 조회
    * @param input 조회할 지역 조건
@@ -48,7 +55,7 @@ export class OpenDataController {
   }
 
   /**
-   * 시가총액과 주식 정보를 조회합니다.
+   * [금융위원회] 시가총액과 주식 정보를 조회합니다.
    *
    * @summary 시가총액 및 주식 정보 조회
    *
@@ -65,7 +72,7 @@ export class OpenDataController {
   }
 
   /**
-   * 오늘 날씨를 조회합니다.
+   * [기상청] 오늘 날씨를 조회합니다.
    *
    * @summary 기상청 오늘 날씨 조회
    *
