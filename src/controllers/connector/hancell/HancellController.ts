95631a1d6a8d7957ae4e0e1a35a9b6702d41ceec
kakasoo <kscodebase@gmail.com>
2024-06-25 10:21:53 +0900

    docs: add comment and standalone decorator to hancell file read connector #26

--- src/controllers/connector/hancell/HancellController.ts
+++ src/controllers/connector/hancell/HancellController.ts
@@ -1,5 +1,6 @@
 import core, { TypedBody } from "@nestia/core";
 import { Controller } from "@nestjs/common";
+import { Standalone } from "@wrtn/decorators";
 
 import { IHancell } from "@wrtn/connector-api/lib/structures/connector/hancell/IHancell";
 
@@ -7,6 +8,14 @@ import { HancellProvider } from "../../../providers/connector/hancell/HancellPro
 
 @Controller("connector/hancell")
 export class HancellController {
+  /**
+   * 한셀 파일을 읽습니다.
+   *
+   * @summary 한셀 파일 읽기
+   * @param input 읽을 한셀 파일 정보
+   * @returns 한셀 파일 정보
+   */
+  @Standalone()
   @core.TypedRoute.Post("read")
   async read(
     @TypedBody() input: IHancell.IReadHancellInput,
