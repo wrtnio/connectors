47f66c9969dc97424ffa042663fd5c4e505af5c6
kakasoo <kscodebase@gmail.com>
2024-07-02 01:41:05 +0900

    docs: SDK 함수 명 변경 및 주석 추가

--- test/features/api/connector/google_slides/test_api_connector_google_slides_export_presentation.ts
+++ test/features/api/connector/google_slides/test_api_connector_google_slides_export_presentation.ts
@@ -4,18 +4,17 @@ import CApi from "@wrtn/connector-api/lib/index";
 
 import { ConnectorGlobal } from "../../../../../src/ConnectorGlobal";
 
-export const test_api_connector_google_slides_export_presentation = async (
-  connection: CApi.IConnection,
-) => {
-  const presentationId = "1b_8z1T4FdAxxJwv5wDgATcZwp17lldQLMN4Nb3C8PsE"; // 테스트 용 파일
-  const res =
-    await CApi.functional.connector.google_slides.presentations.$export.power_point.$export(
-      connection,
-      presentationId,
-      {
-        secretKey: ConnectorGlobal.env.GOOGLE_TEST_SECRET,
-      },
-    );
+export const test_api_connector_google_slides_export_presentation_power_point =
+  async (connection: CApi.IConnection) => {
+    const presentationId = "1b_8z1T4FdAxxJwv5wDgATcZwp17lldQLMN4Nb3C8PsE"; // 테스트 용 파일
+    const res =
+      await CApi.functional.connector.google_slides.presentations.$export.power_point.powerPoint(
+        connection,
+        presentationId,
+        {
+          secretKey: ConnectorGlobal.env.GOOGLE_TEST_SECRET,
+        },
+      );
 
-  typia.assertEquals(res);
-};
+    typia.assertEquals(res);
+  };
