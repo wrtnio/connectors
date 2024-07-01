620a8337579d24b9490401c3d085f282320d1237
Jeongho Nam <samchon.github@gmail.com>
2024-07-01 18:19:23 +0900

    Exact naming

--- website/build/swagger.js
+++ website/build/swagger.js
@@ -6,7 +6,7 @@ const main = () => {
     stdio: "inherit",
     cwd: `${__dirname}/../..`,
   });
-  for (const name of ["swagger", "openapi-keyword", "openai-positional", "migrate", "version"])
+  for (const name of ["swagger", "openai-keyword", "openai-positional", "migrate", "version"])
     fs.copyFileSync(
       `${__dirname}/../../packages/api/${name}.json`,
       `${__dirname}/../out/swagger/${name}.json`,
