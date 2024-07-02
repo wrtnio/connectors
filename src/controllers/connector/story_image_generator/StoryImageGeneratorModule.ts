ce6dd32d80095dbc8d6ec4d9590856b8432cca33
Leo <seunghwa333@gmail.com>
2024-07-02 18:22:29 +0900

    feat: change dall-e-3 to sdxl-beta story-image-generator

--- src/controllers/connector/story_image_generator/StoryImageGeneratorModule.ts
+++ src/controllers/connector/story_image_generator/StoryImageGeneratorModule.ts
@@ -1,13 +1,12 @@
-import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 
 import { StoryImageGeneratorProvider } from "../../../providers/connector/story_image_generator/StoryImageGeneratorProvider";
 import { OpenAIModule } from "../../../providers/open_ai/OpenAIModule";
-import { AwsModule } from "../aws/AwsModule";
+import { StableDiffusionBetaModule } from "../stable_diffustion_beta/StableDiffusionBetaModule";
 import { StoryImageGeneratorController } from "./StoryImageGeneratorController";
 
 @Module({
-  imports: [OpenAIModule, AwsModule, HttpModule],
+  imports: [StableDiffusionBetaModule, OpenAIModule],
   providers: [StoryImageGeneratorProvider],
   controllers: [StoryImageGeneratorController],
 })
