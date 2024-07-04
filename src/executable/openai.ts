227c053dfc0b981dea6f619cb16de0a3e48624ce
Jeongho Nam <samchon.github@gmail.com>
2024-07-04 15:01:19 +0900

    OpenAI schema separation to LLM and Human roles

--- src/executable/openai.ts
+++ src/executable/openai.ts
@@ -1,41 +1,52 @@
-import { IMigrateDocument, OpenApi } from "@samchon/openapi";
 import cp from "child_process";
 import fs from "fs";
 import typia from "typia";
+import {
+  IOpenAiDocument,
+  ISwagger,
+  OpenAiComposer,
+  OpenAiTypeChecker,
+} from "@wrtnio/openai-function-schema";
 
 import { ConnectorConfiguration } from "../ConnectorConfiguration";
-import { IOpenAiDocument } from "../services/openai/IOpenAiDocument";
-import { OpenAiConverter } from "../services/openai/OpenAiConverter";
+import { IMigrateDocument, OpenApi } from "@samchon/openapi";
 
 const main = async (): Promise<void> => {
   const location: string = `${ConnectorConfiguration.ROOT}/packages/api`;
   if (false === fs.existsSync(`${location}/swagger.json`))
     cp.execSync("npx nestia swagger", { stdio: "inherit" });
 
-  const document: OpenApi.IDocument = typia.assert<OpenApi.IDocument>(
+  const swagger: ISwagger = typia.assert<ISwagger>(
     JSON.parse(await fs.promises.readFile(`${location}/swagger.json`, "utf-8")),
   );
-  const migrated: IMigrateDocument = OpenApi.migrate(document);
-  for (const isKeywordParameter of [false, true]) {
-    const openai: IOpenAiDocument = OpenAiConverter.convert({ 
-      document, 
-      migrated, 
-      options: { isKeywordParameter }, 
+  const migrate: IMigrateDocument = OpenApi.migrate(swagger);
+
+  for (const keyword of [false, true]) {
+    const openai: IOpenAiDocument = OpenAiComposer.document({
+      swagger,
+      migrate,
+      options: {
+        keyword,
+        separate: (s) =>
+          OpenAiTypeChecker.isString(s) &&
+          (s["x-wrtn-secret-key"] !== undefined ||
+            s.contentMediaType !== undefined),
+      },
     });
     await fs.promises.writeFile(
-      `${location}/openai-${isKeywordParameter ? "keyword" : "positional"}.json`,
-      JSON.stringify(openai),
+      `${location}/openai-${keyword ? "keyword" : "positional"}.json`,
+      JSON.stringify(openai, null, 2),
       "utf8",
     );
   }
   await fs.promises.writeFile(
     `${location}/migrate.json`,
-    JSON.stringify(migrated),
+    JSON.stringify(migrate),
     "utf8",
   );
   await fs.promises.writeFile(
     `${location}/version.json`,
-    JSON.stringify({ version: document.info?.version }),
+    JSON.stringify({ version: swagger.info?.version }),
     "utf8",
   );
 };
