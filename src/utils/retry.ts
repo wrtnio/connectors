6b0902a717b1dd86a8f00537c4626f0d86af0239
kakasoo <kscodebase@gmail.com>
2024-07-04 14:37:16 +0900

    fix: `retry` 함수 내에 Logger 생성하여 사용

--- src/utils/retry.ts
+++ src/utils/retry.ts
@@ -1,7 +1,10 @@
+import { Logger } from "@nestjs/common";
+
 export function retry<T extends any[], ReturnType>(
   fn: (...args: T) => ReturnType,
   count: number = 5, // default로 5번까지는 재실행되게 한다.
 ) {
+  const logger = new Logger("utils/retry");
   return async function (...args: T): Promise<Awaited<ReturnType>> {
     let attempts = 0;
     while (attempts < count) {
@@ -12,7 +15,7 @@ export function retry<T extends any[], ReturnType>(
         if (attempts >= count) {
           throw error;
         }
-        console.warn(error);
+        logger.debug(JSON.stringify(error));
       }
     }
 
