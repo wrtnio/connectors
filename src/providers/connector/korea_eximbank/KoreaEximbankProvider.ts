096aa92f9fbf290ae07a6ec83c827c5ab9cee088
kakasoo <kscodebase@gmail.com>
2024-07-08 13:09:31 +0900

    fix: use http instead of https

--- src/providers/connector/korea_eximbank/KoreaEximbankProvider.ts
+++ src/providers/connector/korea_eximbank/KoreaEximbankProvider.ts
@@ -4,7 +4,7 @@ import { ConnectorGlobal } from "../../../ConnectorGlobal";
 export namespace KoreaEximbankProvider {
   export async function getExchange() {
     try {
-      const baseUrl = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON`;
+      const baseUrl = `http://www.koreaexim.go.kr/site/program/financial/exchangeJSON`;
       const authKey = ConnectorGlobal.env.KOREA_EXIM_BANK_API_KEY;
 
       const url = `${baseUrl}?authkey=${authKey}&data=AP01`;
