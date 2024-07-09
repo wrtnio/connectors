3264ffc9e72c508fa9834f7f525fb4ae9fed7011
Leo <seunghwa333@gmail.com>
2024-07-09 13:58:48 +0900

    feat: add career category search

--- src/providers/connector/google_search/GoogleSearchProvider.ts
+++ src/providers/connector/google_search/GoogleSearchProvider.ts
@@ -6,47 +6,117 @@ import { IGoogleSearch } from "@wrtn/connector-api/lib/structures/connector/goog
 import { ConnectorGlobal } from "../../../ConnectorGlobal";
 import { makeQuery } from "../../../utils/generate-search-query.util";
 
+const defaultParams = {
+  engine: "google",
+  api_key: ConnectorGlobal.env.SERP_API_KEY,
+  safe: "active",
+  hl: "ko",
+  gl: "kr",
+};
+
 @Injectable()
 export class GoogleSearchProvider {
-  async search(
+  async searchResult(
     input: IGoogleSearch.IRequest,
-  ): Promise<IGoogleSearch.IResponse[]> {
+    targetSite?: string,
+  ): Promise<IGoogleSearch.IResponse[] | string | string> {
     try {
-      const defaultParams = {
-        engine: "google",
-        api_key: ConnectorGlobal.env.SERP_API_KEY,
-        safe: "active",
-        hl: "ko",
-        gl: "kr",
-      };
+      const maxResultPerPage: number = 10;
+      let start: number = 0;
 
-      const searchQuery = makeQuery(
+      let searchQuery = makeQuery(
         input.andKeywords,
         input.orKeywords ?? [],
         input.notKeywords ?? [],
       );
-      const res = await getJson({
-        ...defaultParams,
-        q: searchQuery,
-      });
-      const output = res["organic_results"];
+
+      const output: IGoogleSearch.IResponse[] | string = [];
 
       /**
-       * output의 구조가 검색어 별로 다 다르기 때문에 any로 선언
+       * 한 페이지에 최대 10개의 결과물이 response되기 때문에
+       * 사용자가 원하는 결과물의 갯수가 10개 보다 많은 경우 처리하기 위함.
        */
-      const results: IGoogleSearch.IResponse[] = output.map((o: any) => {
-        return {
-          title: o.title,
-          link: o.link,
-          snippet: o.snippet,
-          thumbnail: o.thumbnail,
+      while (output.length < input.max_results) {
+        const params = {
+          ...defaultParams,
+          q: searchQuery,
+          start: start,
+          num: maxResultPerPage,
+          ...(targetSite && { as_sitesearch: targetSite }),
         };
-      });
+        console.log(params);
+        const res = await getJson(params);
+        const results = res["organic_results"];
+        console.log(results);
 
-      return results;
+        if (results === undefined) {
+          return "검색 결과가 없습니다.";
+        }
+
+        for (const result of results) {
+          if (output.length === input.max_results) {
+            break;
+          }
+
+          const data: IGoogleSearch.IResponse = {
+            title: result.title,
+            link: result.link,
+            snippet: result.snippet,
+            thumbnail: result.thumbnail,
+          };
+
+          output.push(data);
+        }
+
+        /**
+         * 검색 결과가 10개보다 적은 경우
+         */
+        if (results.length < maxResultPerPage) {
+          break;
+        }
+        start += maxResultPerPage;
+      }
+
+      return output;
     } catch (error) {
       console.error(JSON.stringify(error));
       throw error;
     }
+  }
+
+  async search(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input);
+  }
+
+  async searchForWanted(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input, "https://www.wanted.co.kr");
+  }
+
+  async searchForIncruit(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input, "https://www.incruit.com");
+  }
+
+  async searchForSaramin(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input, "https://www.saramin.co.kr/zf_user");
+  }
+
+  async searchForJumpit(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input, "https://www.jumpit.co.kr");
+  }
+
+  async searchForCareerly(
+    input: IGoogleSearch.IRequest,
+  ): Promise<IGoogleSearch.IResponse[] | string> {
+    return this.searchResult(input, "https://careerly.co.kr");
   }
 }
