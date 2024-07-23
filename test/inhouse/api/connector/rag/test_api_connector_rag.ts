aaaa91ea9066625af15c63e436e01fe099cf4b56
Leo <seunghwa333@gmail.com>
2024-07-23 11:35:09 +0900

    chore: delete console log

--- test/inhouse/api/connector/rag/test_api_connector_rag.ts
+++ test/inhouse/api/connector/rag/test_api_connector_rag.ts
@@ -4,35 +4,31 @@ import CApi from "@wrtn/connector-api/lib/index";
 import { IRag } from "@wrtn/connector-api/lib/structures/connector/rag/IRag";
 
 export const test_api_connector_rag = async (connection: CApi.IConnection) => {
-  /**
-   * Pdf 형식
-   */
-  const analyzePdfInput: IRag.IAnalyzeInput[] = [
+  const analyzeInput: IRag.IAnalyzeInput[] = [
     {
       fileUrl: `https://studio-api-bucket.s3.ap-northeast-2.amazonaws.com/rag-test-2.pdf`,
       fileType: "pdf",
     },
     {
-      fileUrl: `https://studio-api-bucket.s3.ap-northeast-2.amazonaws.com/rag-test.pdf`,
-      fileType: "pdf",
+      fileUrl: `https://studio-api-bucket.s3.ap-northeast-2.amazonaws.com/rag-hwp-test.hwp`,
+      fileType: "hwp",
     },
   ];
-  const analyzePdf = await CApi.functional.connector.rag.analyze(
+  const analyzeOutput = await CApi.functional.connector.rag.analyze(
     connection,
-    analyzePdfInput,
+    analyzeInput,
   );
-  typia.assertEquals<IRag.IAnalysisOutput>(analyzePdf);
+  typia.assertEquals<IRag.IAnalysisOutput>(analyzeOutput);
 
   const generateInput: IRag.IGenerateInput = {
     query:
-      "지구 온난화(열 스트레스)와 미세플라스틱이 다양한 산호초 종에 미치는 세 가지 실험내용을 요약 분석해 줘. 그리고 마이크로 플라스틱이 잎이 많은 식물의 생장에 미치는 영향을 분석 및 비교해줘. 각각의 비교를 할 때 대제목을 생성해줘.",
+      "지구 온난화(열 스트레스)와 미세플라스틱이 다양한 산호초 종에 미치는 세 가지 실험내용을 요약 분석해 줘. 그리고 대규모 언어 모델(LLM)의 맥락에서 오픈소스 소프트웨어의 국가별 다양화와 발전을 요약해줘. 각각의 비교를 할 때 대제목을 생성해줘.",
   };
-  const chatId = analyzePdf.chatId;
+  const chatId = analyzeOutput.chatId;
   const generateOutput = await CApi.functional.connector.rag.generate(
     connection,
     generateInput,
     chatId,
   );
-  console.log("result", generateOutput);
   typia.assertEquals<IRag.IGenerateOutput>(generateOutput);
 };
