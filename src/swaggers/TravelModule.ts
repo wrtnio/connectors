5c99da17b9f3b0c06d15d55b2b7f8fcdcb9ee7e9
Leo <seunghwa333@gmail.com>
2024-07-24 18:20:09 +0900

    feat: add google hotel nestia config

--- src/swaggers/TravelModule.ts
+++ src/swaggers/TravelModule.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { KakaoMapModule } from "../controllers/connector/kakao_map/KakaoMapModule";
 import { KakaoNaviModule } from "../controllers/connector/kakao_navi/KakaoNaviModule";
+import { GoogleHotelModule } from "../controllers/connector/google_hotel/GoogleHotelModule";
 
 @Module({
-  imports: [KakaoMapModule, KakaoNaviModule],
+  imports: [KakaoMapModule, KakaoNaviModule, GoogleHotelModule],
 })
 export class TravelModule {}
