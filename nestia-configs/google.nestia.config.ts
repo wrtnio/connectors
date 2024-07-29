fb4f5f7d7d8e140be67c27024c75ac838725754b
Asher <asher@wrtn.io>
2024-07-29 16:27:10 +0900

    구글 스토리지 swagger 로 변경

--- nestia-configs/google.nestia.config.ts
+++ nestia-configs/google.nestia.config.ts
@@ -11,7 +11,7 @@ const NESTIA_CONFIG: sdk.INestiaConfig = {
     },
     beautify: true,
     decompose: true,
-    output: "packages/api/connectors/commerce.swagger.json",
+    output: "packages/api/connectors/google-storage.swagger.json",
     servers: [
       {
         url: "https://studio-connector-api.wrtn.ai",
