992e4cbeb41d75d246ed94d2633f4d14126e4cc2
kakasoo <kscodebase@gmail.com>
2024-08-02 14:51:32 +0900

    docs: add kakao-talk service name in korean

--- src/controllers/connector/kakao_talk/KakaoTalkController.ts
+++ src/controllers/connector/kakao_talk/KakaoTalkController.ts
@@ -11,15 +11,15 @@ import { retry } from "../../../utils/retry";
 @Controller("connector/kakao-talk")
 export class KakaoTalkController {
   /**
-   * Sends a text type KakaoTalk message to a friend
+   * Sends a text type KakaoTalk(카카오톡) message to a friend
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to a KakaoTalk friend
+   * @summary Send a message to a KakaoTalk(카카오톡) friend
    * @param input Conditions for sending the message
    * @returns Response and failure information
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @RouteIcon(
     `https://ecosystem-connector.s3.ap-northeast-2.amazonaws.com/icon/kakaoTalk.svg`,
@@ -32,17 +32,17 @@ export class KakaoTalkController {
   }
 
   /**
-   * Sends a commerce type message to myself on KakaoTalk
+   * Sends a commerce type message to myself on KakaoTalk(카카오톡)
    *
-   * When sending a KakaoTalk message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk API specifications.
+   * When sending a KakaoTalk(카카오톡) message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk(카카오톡) API specifications.
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to myself on KakaoTalk
+   * @summary Send a message to myself on KakaoTalk(카카오톡)
    * @param input Conditions for sending the message
    * @returns Response code
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -56,17 +56,17 @@ export class KakaoTalkController {
   }
 
   /**
-   * Sends a location type message to myself on KakaoTalk
+   * Sends a location type message to myself on KakaoTalk(카카오톡)
    *
-   * When sending a KakaoTalk message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk API specifications.
+   * When sending a KakaoTalk(카카오톡) message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk(카카오톡) API specifications.
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to myself on KakaoTalk
+   * @summary Send a message to myself on KakaoTalk(카카오톡)
    * @param input Conditions for sending the message
    * @returns Response code
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -80,17 +80,17 @@ export class KakaoTalkController {
   }
 
   /**
-   * Sends a list type message to myself on KakaoTalk
+   * Sends a list type message to myself on KakaoTalk(카카오톡)
    *
-   * When sending a KakaoTalk message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk API specifications.
+   * When sending a KakaoTalk(카카오톡) message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk(카카오톡) API specifications.
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to myself on KakaoTalk
+   * @summary Send a message to myself on KakaoTalk(카카오톡)
    * @param input Conditions for sending the message
    * @returns Response code
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -104,17 +104,17 @@ export class KakaoTalkController {
   }
 
   /**
-   * Sends a feed type message to myself on KakaoTalk
+   * Sends a feed type message to myself on KakaoTalk(카카오톡)
    *
-   * When sending a KakaoTalk message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk API specifications.
+   * When sending a KakaoTalk(카카오톡) message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk(카카오톡) API specifications.
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to myself on KakaoTalk
+   * @summary Send a message to myself on KakaoTalk(카카오톡)
    * @param input Conditions for sending the message
    * @returns Response code
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -128,17 +128,17 @@ export class KakaoTalkController {
   }
 
   /**
-   * Sends a text type message to myself on KakaoTalk
+   * Sends a text type message to myself on KakaoTalk(카카오톡)
    *
-   * When sending a KakaoTalk message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk API specifications.
+   * When sending a KakaoTalk(카카오톡) message, there are buttons. If you want to add a link to the button, you should use a URL starting with `https://studio-pro.wrtn.ai/` or a redirect link. If the link starts with `https://studio-pro.wrtn.ai/`, the page will be viewed, otherwise, it will redirect to the new link. This is because only links registered in our domain are allowed according to the KakaoTalk(카카오톡) API specifications.
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Send a message to myself on KakaoTalk
+   * @summary Send a message to myself on KakaoTalk(카카오톡)
    * @param input Conditions for sending the message
    * @returns Response code
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -152,16 +152,16 @@ export class KakaoTalkController {
   }
 
   /**
-   * Adds an event to the KakaoTalk calendar
+   * Adds an event to the KakaoTalk(카카오톡) calendar
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Add an event to the KakaoTalk calendar
+   * @summary Add an event to the KakaoTalk(카카오톡) calendar
    * @param input Input conditions for creating an event
    *
    * @returns Created event ID condition
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -175,15 +175,15 @@ export class KakaoTalkController {
   }
 
   /**
-   * Retrieves the list of friends on KakaoTalk
+   * Retrieves the list of friends on KakaoTalk(카카오톡)
    *
-   * KakaoTalk is a mobile messenger application in South Korea, which also provides various additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application in South Korea, which also provides various additional services.
    *
-   * @summary Retrieve the list of friends on KakaoTalk
+   * @summary Retrieve the list of friends on KakaoTalk(카카오톡)
    * @param input Conditions for retrieving the friend list
    * @returns Retrieved friend list
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -197,7 +197,7 @@ export class KakaoTalkController {
   }
 
   /**
-   * Retrieves KakaoTalk calendar events.
+   * Retrieves KakaoTalk(카카오톡) calendar events.
    *
    * The user needs to provide the calendar ID as an input parameter.
    * If no calendar is provided, it defaults to retrieving the user's own calendar.
@@ -206,13 +206,13 @@ export class KakaoTalkController {
    * The conditions for retrieving events include specifying the period for which events are to be fetched.
    * This connector is designed to view data for either a week or a month.
    *
-   * KakaoTalk is a mobile messaging application in South Korea, and it also provides additional services.
+   * KakaoTalk(카카오톡) is a mobile messaging application in South Korea, and it also provides additional services.
    *
-   * @summary Retrieve KakaoTalk calendar events
+   * @summary Retrieve KakaoTalk(카카오톡) calendar events
    * @param input DTO for event retrieval.
    * @returns DTO containing event information.
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -226,19 +226,19 @@ export class KakaoTalkController {
   }
 
   /**
-   * Retrieves all KakaoTalk calendar lists
+   * Retrieves all KakaoTalk(카카오톡) calendar lists
    *
    * There are two types of calendars: your primary calendars and calendars you are subscribed to.
    * All Kakao users have their own personal calendars, so there will be at least one calendar.
    * The primary calendar has an ID of `primary`, which is the user's own calendar.
    *
-   * KakaoTalk is a mobile messenger application from South Korea that also provides additional services.
+   * KakaoTalk(카카오톡) is a mobile messenger application from South Korea that also provides additional services.
    *
-   * @summary Retrieve KakaoTalk calendar lists
+   * @summary Retrieve KakaoTalk(카카오톡) calendar lists
    * @param input Request DTO to retrieve calendars.
    * @returns Calendar list object.
    *
-   * @tag KakaoTalk
+   * @tag KakaoTalk(카카오톡)
    */
   @Standalone()
   @RouteIcon(
@@ -252,7 +252,7 @@ export class KakaoTalkController {
   }
 
   /**
-   * Issues a KakaoTalk access token
+   * Issues a KakaoTalk(카카오톡) access token
    *
    * @internal
    *
@@ -266,7 +266,7 @@ export class KakaoTalkController {
   }
 
   /**
-   * Refreshes the KakaoTalk access token
+   * Refreshes the KakaoTalk(카카오톡) access token
    *
    * @internal
    *
