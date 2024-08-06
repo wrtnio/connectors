01cef0abc3e49479c8ed0191a4a85e9d7d9a4f8e
kakasoo <kscodebase@gmail.com>
2024-08-06 18:56:42 +0900

    test: add test of project list with pagination

--- test/features/api/connector/jira/test_api_connector_get_projects.ts
+++ test/features/api/connector/jira/test_api_connector_get_projects.ts
@@ -17,3 +17,22 @@ export const test_api_connector_jira_get_projects = async (
   typia.assertEquals(res);
   return res.values;
 };
+
+export const test_api_connector_jira_get_projects_2 = async (
+  connection: CApi.IConnection,
+) => {
+  const res = await CApi.functional.connector.jira.get_projects.getProjects(
+    connection,
+    {
+      email: "studio@wrtn.io",
+      apiToken: ConnectorGlobal.env.JIRA_TEST_SECRET,
+      domain: "https://wrtn-ecosystem.atlassian.net",
+      maxResults: 50,
+      startAt: 0,
+      orderBy: "name",
+    },
+  );
+
+  typia.assertEquals(res);
+  return res.values;
+};
