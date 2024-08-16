2ae1b4f8a0b730ce15a107f3372171e91c6a4da7
kakasoo <kscodebase@gmail.com>
2024-08-16 11:56:44 +0900

    feat: add provider login for searching users in github

--- src/providers/connector/github/GithubProvider.ts
+++ src/providers/connector/github/GithubProvider.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { IGithub } from "@wrtn/connector-api/lib/structures/connector/github/IGithub";
+import axios from "axios";
 
 @Injectable()
-export class GithubProvider {}
+export class GithubProvider {
+  async searchUser(input: IGithub.ISearchUserInput) {
+    const url = `https://api.github.com/search/users?q=${input.q}`;
+    const res = await axios.get(url);
+    return res.data;
+  }
+}
