0e1de417dd848ff0c61733883cb12d8b5d71feff
kakasoo <kscodebase@gmail.com>
2024-08-16 12:52:53 +0900

    feat: get list of user's repositories api in github

--- src/providers/connector/github/GithubProvider.ts
+++ src/providers/connector/github/GithubProvider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { IGithub } from "@wrtn/connector-api/lib/structures/connector/github/IGithub";
 import axios from "axios";
+import { createQueryParameter } from "../../../utils/CreateQueryParameter";
 
 @Injectable()
 export class GithubProvider {
@@ -20,6 +21,20 @@ export class GithubProvider {
     input: IGithub.IGetUserProfileInput,
   ): Promise<IGithub.IGetUserProfileOutput> {
     const url = `https://api.github.com/users/${input.username}`;
+    const res = await axios.get(url, {
+      headers: {
+        Accept: "application/vnd.github+json",
+      },
+    });
+    return res.data;
+  }
+
+  async getUserRepositories(
+    input: IGithub.IGetUserRepositoryInput,
+  ): Promise<IGithub.IGetUserRepositoryOutput> {
+    const { username, ...rest } = input;
+    const queryParameter = createQueryParameter(rest);
+    const url = `https://api.github.com/users/${username}/repos?${queryParameter}`;
     const res = await axios.get(url, {
       headers: {
         Accept: "application/vnd.github+json",
