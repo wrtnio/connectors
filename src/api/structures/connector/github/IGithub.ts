c263c52786091150e1468e75f9d14afffca9f62e
kakasoo <kscodebase@gmail.com>
2024-08-16 12:10:42 +0900

    feat: add user profile api in github

--- src/api/structures/connector/github/IGithub.ts
+++ src/api/structures/connector/github/IGithub.ts
@@ -23,6 +23,94 @@ export namespace IGithub {
     order?: ("desc" | "asc") & tags.Default<"desc">;
   }
 
+  export interface IGetUserProfileOutput
+    extends Pick<User, "id" | "login" | "avatar_url" | "type"> {
+    /**
+     * @title name
+     * It means the actual name that the user has written, not the user's nickname.
+     */
+    name: string;
+
+    /**
+     * @title comany name
+     *
+     * As the name of the company,
+     * it cannot be said to be the exact name listed as the business operator because it was written by the user himself.
+     * Also, we cannot guarantee that the user wrote the company name.
+     * Sometimes the user jokingly writes down strange names.
+     */
+    company: string | null;
+
+    /**
+     * @title blog
+     *
+     * Indicates the blog address.
+     */
+    blog: (string & tags.Format<"uri">) | "" | null;
+
+    /**
+     * @title location
+     *
+     * It means the location of the user.
+     * Usually, I write the country down, but the user can jokingly record the strange location.
+     */
+    location: string | null;
+
+    /**
+     * @title email address
+     */
+    email: string | null;
+
+    /**
+     * @title bio
+     *
+     * Write down what the user wants to say or a history.
+     */
+    bio: string | null;
+
+    /**
+     * @title twitter_username
+     */
+    twitter_username: string | null;
+
+    /**
+     * @title count of public repos
+     */
+    public_repos: number & tags.Type<"uint32">;
+
+    /**
+     * @title count of public gists
+     */
+    public_gists: number & tags.Type<"uint32">;
+
+    /**
+     * @title count of followers
+     */
+    followers: number & tags.Type<"uint32">;
+
+    /**
+     * @title count of follwing
+     */
+    following: number & tags.Type<"uint32">;
+
+    /**
+     * @title created_at
+     */
+    created_at: string & tags.Format<"date-time">;
+
+    /**
+     * @title updated_at
+     */
+    updated_at: string & tags.Format<"date-time">;
+  }
+
+  export interface IGetUserProfileInput {
+    /**
+     * @title username
+     */
+    username: string;
+  }
+
   export interface ISearchUserOutput {
     /**
      * @title total count of users
@@ -62,6 +150,9 @@ export namespace IGithub {
      * @title login
      *
      * This means the user's nickname.
+     * In github, nicknames are unique at least until that user changes their own nickname.
+     * This means that only one person can own the nickname at a time.
+     * Therefore, it may be important to know the exact nickname because the github API calls the appi using the user's nickname.
      */
     login: string;
 
