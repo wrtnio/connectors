6fadd7f8e05c39ed9e02a3203f7b56be343ece88
Leo <seunghwa333@gmail.com>
2024-09-13 18:26:29 +0900

    feat: add description for controller

--- src/controllers/connector/ai_search/AISearchController.ts
+++ src/controllers/connector/ai_search/AISearchController.ts
@@ -8,6 +8,14 @@ import { IAISearch } from "@wrtn/connector-api/lib/structures/connector/ai_searc
 @Controller("connector/ai-search")
 export class AISearchController {
   constructor(private readonly aiSearchProvider: AISearchProvider) {}
+  /**
+   * AI 검색을 통해 검색 결과를 반환합니다.
+   *
+   * @summary AI 검색
+   *
+   * @param input 검색에 필요한 조건
+   * @returns
+   */
   @core.TypedRoute.Post("")
   // @RouteIcon(
   //   "https://ecosystem-connector.s3.ap-northeast-2.amazonaws.com/icon/fulls/GoogleMap_full.svg",
