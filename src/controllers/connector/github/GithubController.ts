77a161c0a3f77d04b30f9f8ed548f310f5effea9
kakasoo <kscodebase@gmail.com>
2024-09-17 22:52:02 +0900

    feat: add comments for getting collaborators

--- src/controllers/connector/github/GithubController.ts
+++ src/controllers/connector/github/GithubController.ts
@@ -191,6 +191,19 @@ export class GithubController {
     return data;
   }
 
+  /**
+   * List repository collaborators
+   *
+   * For organization-owned repositories, the list of collaborators includes outside collaborators,
+   * organization members that are direct collaborators, organization members with access through team memberships,
+   * organization members with access through default organization permissions, and organization owners.
+   * Organization members with write, maintain, or admin privileges on the organization-owned repository can use this endpoint.
+   * Team members will include the members of child teams.
+   *
+   * @summary List repository collaborators
+   * @param input
+   * @returns
+   */
   @core.TypedRoute.Post("repos/get-collaborators")
   async getCollaborators(
     @TypedBody() input: IGithub.IGetCollaboratorInput,
