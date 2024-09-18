b58b04a9559dcd2ade1d88e9c3791b5d6c181c7d
kakasoo <kscodebase@gmail.com>
2024-09-18 19:41:47 +0900

    test: add test code for reviewers and reviews connector

--- test/features/api/connector/github/test_api_connector_github_read_pull_request_reviews.ts
+++ test/features/api/connector/github/test_api_connector_github_read_pull_request_reviews.ts
@@ -1,6 +1,7 @@
 import CApi from "@wrtn/connector-api/lib/index";
 import typia from "typia";
 import { ConnectorGlobal } from "../../../../../src/ConnectorGlobal";
+import assert from "assert";
 
 export async function test_api_connector_github_repository_read_pull_request_reviews(
   connection: CApi.IConnection,
@@ -34,4 +35,38 @@ export async function test_api_connector_github_repository_read_pull_request_rev
 
     typia.assert(reviews);
   }
+}
+
+export async function test_api_connector_github_repository_read_pull_request_reviews_2(
+  connection: CApi.IConnection,
+) {
+  const reviews =
+    await CApi.functional.connector.github.repositories.pull_requests.get_reviews.readReviews(
+      connection,
+      {
+        owner: "wrtnio",
+        repo: "connectors",
+        pull_number: 18 as const,
+        secretKey: ConnectorGlobal.env.G_GITHUB_TEST_SECRET,
+      },
+    );
+
+  typia.assert(reviews);
+
+  const requested_reviewers =
+    await CApi.functional.connector.github.repositories.pull_requests.get_requested_reviewers.readPullRequestRequestedReviewers(
+      connection,
+      {
+        owner: "wrtnio",
+        repo: "connectors",
+        pull_number: 18 as const,
+        secretKey: ConnectorGlobal.env.G_GITHUB_TEST_SECRET,
+      },
+    );
+
+  typia.assert(requested_reviewers);
+
+  assert(reviews.result.length === 1);
+  assert(requested_reviewers.users.length === 0);
+  assert(requested_reviewers.teams.length === 0);
 }
