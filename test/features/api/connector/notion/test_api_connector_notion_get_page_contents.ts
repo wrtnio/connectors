1e768741c3de90262fa5432363b2d97e0272044f
kakasoo <kscodebase@gmail.com>
2024-10-01 23:03:38 +0900

    fix: 너무 무거운 테스트 코드를 대체

--- test/features/api/connector/notion/test_api_connector_notion_get_page_contents.ts
+++ test/features/api/connector/notion/test_api_connector_notion_get_page_contents.ts
@@ -2,30 +2,6 @@ import CApi from "@wrtn/connector-api/lib/index";
 import typia from "typia";
 import { ConnectorGlobal } from "../../../../../src/ConnectorGlobal";
 
-export const test_api_connector_notion_get_page_contents = async (
-  connection: CApi.IConnection,
-) => {
-  const pages = await CApi.functional.connector.notion.get.page.readPageList(
-    connection,
-    {
-      secretKey: ConnectorGlobal.env.NOTION_TEST_SECRET,
-    },
-  );
-
-  for await (const page of pages.slice(0, 10)) {
-    const res =
-      await CApi.functional.connector.notion.get.page.contents.readPageContents(
-        connection,
-        {
-          block_id: page.pageId as string,
-          secretKey: ConnectorGlobal.env.NOTION_TEST_SECRET,
-        },
-      );
-
-    typia.assertEquals(res);
-  }
-};
-
 export const test_api_connector_notion_get_page_contents_by_page_id = async (
   connection: CApi.IConnection,
 ) => {
