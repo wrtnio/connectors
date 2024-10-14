5a0cf5f61cf48e46b10e555c7093cd75fa5f11a7
kakasoo <kscodebase@gmail.com>
2024-10-14 19:30:09 +0900

    Revert "Add artist retrieval feature with pagination and typia tags"
    This reverts commit 241c383c445cb14803bc2261612a34f333569b15.

--- src/api/structures/connector/spotify/ISpotify.ts
+++ src/api/structures/connector/spotify/ISpotify.ts
@@ -2,58 +2,168 @@ import { tags } from "typia";
 import { ICommon } from "../common/ISecretValue";
 
 export namespace ISpotify {
-  // ... existing code ...
-
-  /** 
-   * @title Get Artists Input
-   * 
-   * Input interface for retrieving artists.
+  /**
+   * @title Get User Playlists Input
+   *
+   * Input interface for retrieving user playlists.
    */
-  export interface IGetArtistsInput extends ICommon.ISecret<"spotify"> {
-    /**
-     * @title Artist IDs
-     * 
-     * A comma-separated list of the Spotify IDs for the artists.
-     */
-    artistIds: string & tags.Format<"csv">;
-
+  export interface IGetUserPlaylistsInput extends ICommon.ISecret<"spotify"> {
     /**
      * @title Limit
-     * 
+     *
      * The number of items to return. Default is 20.
      */
-    limit?: number & tags.Type<"uint64"> & tags.Default<20> & tags.Minimum<0> & tags.Maximum<50>;
+    limit?: number &
+      tags.Type<"uint64"> &
+      tags.Default<20> &
+      tags.Minimum<0> &
+      tags.Maximum<50>;
 
     /**
      * @title Offset
-     * 
+     *
      * The index of the first item to return. Default is 0.
      */
-    offset?: number & tags.Type<"uint64"> & tags.Default<0> & tags.Maximum<100000>;
+    offset?: number &
+      tags.Type<"uint64"> &
+      tags.Default<0> &
+      tags.Maximum<100000>;
   }
 
-  /** 
-   * @title Get Artists Output
-   * 
-   * Output interface for retrieving artists.
+  /**
+   * @title Get User Playlists Output
+   *
+   * Output interface for retrieving user playlists.
    */
-  export interface IGetArtistsOutput {
+  export interface IGetUserPlaylistsOutput {
     /**
-     * @title Artists
-     * 
-     * An array containing the ID, name, and genres of artists.
+     * @title Playlists
+     *
+     * An array containing the ID, name, and track count of playlists.
      */
-    artists: Array<{ id: string; name: string; genres: string[] }>;
+    playlists: Array<{ id: string; name: string; tracks: number }>;
 
     /**
      * @title Pagination Info
-     * 
-     * Information about pagination for the artists.
+     *
+     * Information about pagination for the playlists.
      */
     pagination: {
-      total: number; // Total number of artists
-      limit: number; // Number of artists per page
+      total: number; // Total number of playlists
+      limit: number; // Number of playlists per page
       offset: number; // Current offset
     };
+  }
+
+  /**
+   * @title Get Artist Albums Input
+   *
+   * Input interface for retrieving artist albums.
+   */
+  export interface IGetArtistAlbumsInput extends ICommon.ISecret<"spotify"> {
+    /**
+     * @title Artist ID
+     *
+     * The ID of the artist.
+     */
+    artistId: string;
+  }
+
+  /**
+   * @title Get Artist Albums Output
+   *
+   * Output interface for retrieving artist albums.
+   */
+  export interface IGetArtistAlbumsOutput {
+    /**
+     * @title Albums
+     *
+     * An array containing the ID, name, and release date of albums.
+     */
+    albums: Array<{ id: string; name: string; release_date: string }>;
+  }
+
+  /**
+   * @title Get Current Playing Track Input
+   *
+   * Input interface for retrieving the currently playing track.
+   */
+  export type IGetCurrentPlayingTrackInput = ICommon.ISecret<"spotify">;
+
+  /**
+   * @title Get Current Playing Track Output
+   *
+   * Output interface for retrieving the currently playing track.
+   */
+  export interface IGetCurrentPlayingTrackOutput {
+    /**
+     * @title Track Information
+     *
+     * Contains the ID, name, artist, and album information of the track.
+     */
+    track: { id: string; name: string; artist: string; album: string };
+  }
+
+  /**
+   * @title Create Playlist Input
+   *
+   * Input interface for creating a playlist.
+   */
+  export interface ICreatePlaylistInput extends ICommon.ISecret<"spotify"> {
+    /**
+     * @title User ID
+     *
+     * The ID of the user.
+     */
+    userId: string;
+
+    /**
+     * @title Playlist Name
+     *
+     * The name of the playlist to be created.
+     */
+    playlistName: string;
+  }
+
+  /**
+   * @title Create Playlist Output
+   *
+   * Output interface for creating a playlist.
+   */
+  export interface ICreatePlaylistOutput {
+    /**
+     * @title Playlist ID
+     *
+     * The ID of the created playlist.
+     */
+    playlistId: string;
+  }
+
+  /**
+   * @title Get Recommendations Input
+   *
+   * Input interface for retrieving recommended tracks.
+   */
+  export interface IGetRecommendationsInput extends ICommon.ISecret<"spotify"> {
+    /**
+     * @title Seed Tracks
+     *
+     * An array of seed track IDs for recommendations.
+     */
+    seedTracks: string[];
+  }
+
+  /**
+   * @title Get Recommendations Output
+   *
+   * Output interface for retrieving recommended tracks.
+   */
+  export interface IGetRecommendationsOutput {
+    /**
+     * @title Recommended Tracks
+     *
+     * An array containing the ID, name, and artist information of recommended tracks.
+     */
+    tracks: Array<{ id: string; name: string; artist: string }>;
   }
 }
