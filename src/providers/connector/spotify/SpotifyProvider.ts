69e33de4068be026f4c364187c1fab70b4ead87e
kakasoo <kakasoo@wrtn.io>
2024-10-14 14:23:29 +0900

    Update SpotifyProvider.ts to include pagination parameters

--- src/providers/connector/spotify/SpotifyProvider.ts
+++ src/providers/connector/spotify/SpotifyProvider.ts
@@ -9,80 +9,26 @@ export class SpotifyProvider {
   ): Promise<ISpotify.IGetUserPlaylistsOutput> {
     const response = await axios.get(
       "https://api.spotify.com/v1/me/playlists",
-      { headers: { Authorization: `Bearer ${input.secretKey}` } },
+      {
+        headers: { Authorization: `Bearer ${input.secretKey}` },
+        params: {
+          limit: input.limit ?? 20,
+          offset: input.offset ?? 0,
+        },
+      },
     );
     const playlists = response.data.items.map((playlist: any) => ({
       id: playlist.id,
       name: playlist.name,
       tracks: playlist.tracks.total,
     }));
-    return { playlists };
-  }
-
-  async getArtistAlbums(
-    input: ISpotify.IGetArtistAlbumsInput,
-  ): Promise<ISpotify.IGetArtistAlbumsOutput> {
-    const response = await axios.get(
-      `https://api.spotify.com/v1/artists/${input.artistId}/albums`,
-      { headers: { Authorization: `Bearer ${input.secretKey}` } },
-    );
-    const albums = response.data.items.map((album: any) => ({
-      id: album.id,
-      name: album.name,
-      release_date: album.release_date,
-    }));
-    return { albums };
-  }
-
-  async getCurrentPlayingTrack(
-    input: ISpotify.IGetCurrentPlayingTrackInput,
-  ): Promise<ISpotify.IGetCurrentPlayingTrackOutput> {
-    const response = await axios.get(
-      "https://api.spotify.com/v1/me/player/currently-playing",
-      { headers: { Authorization: `Bearer ${input.secretKey}` } },
-    );
-    const track = response.data.item;
-    return {
-      track: {
-        id: track.id,
-        name: track.name,
-        artist: track.artists[0].name,
-        album: track.album.name,
-      },
+    const pagination = {
+      total: response.data.total,
+      limit: response.data.limit,
+      offset: response.data.offset,
     };
+    return { playlists, pagination };
   }
 
-  async createPlaylist(
-    input: ISpotify.ICreatePlaylistInput,
-  ): Promise<ISpotify.ICreatePlaylistOutput> {
-    const response = await axios.post(
-      `https://api.spotify.com/v1/users/${input.userId}/playlists`,
-      { name: input.playlistName, public: false },
-      {
-        headers: {
-          Authorization: `Bearer ${input.secretKey}`,
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return { playlistId: response.data.id };
-  }
-
-  async getRecommendations(
-    input: ISpotify.IGetRecommendationsInput,
-  ): Promise<ISpotify.IGetRecommendationsOutput> {
-    const response = await axios.get(
-      "https://api.spotify.com/v1/recommendations",
-      {
-        headers: { Authorization: `Bearer ${input.secretKey}` },
-        params: { seed_tracks: input.seedTracks.join(",") },
-      },
-    );
-    const tracks = response.data.tracks.map((track: any) => ({
-      id: track.id,
-      name: track.name,
-      artist: track.artists[0].name,
-    }));
-    return { tracks };
-  }
+  // ... other methods ...
 }
