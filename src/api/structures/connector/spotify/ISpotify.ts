64b8c428d54606fb1b1a1bcc753e67f8b1aca60b
kakasoo <kscodebase@gmail.com>
2024-10-14 14:26:39 +0900

    fix: SecretKey import path

--- src/api/structures/connector/spotify/ISpotify.ts
+++ src/api/structures/connector/spotify/ISpotify.ts
@@ -1,23 +1,23 @@
 import { tags } from "typia";
-import { ICommon } from "./ICommon";
+import { ICommon } from "../common/ISecretValue";
 
 export namespace ISpotify {
   /**
    * @title Get User Playlists Input
-   * 
+   *
    * Input interface for retrieving user playlists.
    */
   export interface IGetUserPlaylistsInput extends ICommon.ISecret<"spotify"> {
     /**
      * @title Limit
-     * 
+     *
      * The number of items to return. Default is 20.
      */
     limit?: number & tags.Default<20>;
 
     /**
      * @title Offset
-     * 
+     *
      * The index of the first item to return. Default is 0.
      */
     offset?: number & tags.Default<0>;
@@ -25,20 +25,20 @@ export namespace ISpotify {
 
   /**
    * @title Get User Playlists Output
-   * 
+   *
    * Output interface for retrieving user playlists.
    */
   export interface IGetUserPlaylistsOutput {
     /**
      * @title Playlists
-     * 
+     *
      * An array containing the ID, name, and track count of playlists.
      */
     playlists: Array<{ id: string; name: string; tracks: number }>;
 
     /**
      * @title Pagination Info
-     * 
+     *
      * Information about pagination for the playlists.
      */
     pagination: {
@@ -50,13 +50,13 @@ export namespace ISpotify {
 
   /**
    * @title Get Artist Albums Input
-   * 
+   *
    * Input interface for retrieving artist albums.
    */
   export interface IGetArtistAlbumsInput extends ICommon.ISecret<"spotify"> {
     /**
      * @title Artist ID
-     * 
+     *
      * The ID of the artist.
      */
     artistId: string;
@@ -64,13 +64,13 @@ export namespace ISpotify {
 
   /**
    * @title Get Artist Albums Output
-   * 
+   *
    * Output interface for retrieving artist albums.
    */
   export interface IGetArtistAlbumsOutput {
     /**
      * @title Albums
-     * 
+     *
      * An array containing the ID, name, and release date of albums.
      */
     albums: Array<{ id: string; name: string; release_date: string }>;
@@ -78,22 +78,20 @@ export namespace ISpotify {
 
   /**
    * @title Get Current Playing Track Input
-   * 
+   *
    * Input interface for retrieving the currently playing track.
    */
-  export interface IGetCurrentPlayingTrackInput extends ICommon.ISecret<"spotify"> {
-    // No additional properties are needed.
-  }
+  export type IGetCurrentPlayingTrackInput = ICommon.ISecret<"spotify">;
 
   /**
    * @title Get Current Playing Track Output
-   * 
+   *
    * Output interface for retrieving the currently playing track.
    */
   export interface IGetCurrentPlayingTrackOutput {
     /**
      * @title Track Information
-     * 
+     *
      * Contains the ID, name, artist, and album information of the track.
      */
     track: { id: string; name: string; artist: string; album: string };
@@ -101,20 +99,20 @@ export namespace ISpotify {
 
   /**
    * @title Create Playlist Input
-   * 
+   *
    * Input interface for creating a playlist.
    */
   export interface ICreatePlaylistInput extends ICommon.ISecret<"spotify"> {
     /**
      * @title User ID
-     * 
+     *
      * The ID of the user.
      */
     userId: string;
 
     /**
      * @title Playlist Name
-     * 
+     *
      * The name of the playlist to be created.
      */
     playlistName: string;
@@ -122,13 +120,13 @@ export namespace ISpotify {
 
   /**
    * @title Create Playlist Output
-   * 
+   *
    * Output interface for creating a playlist.
    */
   export interface ICreatePlaylistOutput {
     /**
      * @title Playlist ID
-     * 
+     *
      * The ID of the created playlist.
      */
     playlistId: string;
@@ -136,13 +134,13 @@ export namespace ISpotify {
 
   /**
    * @title Get Recommendations Input
-   * 
+   *
    * Input interface for retrieving recommended tracks.
    */
   export interface IGetRecommendationsInput extends ICommon.ISecret<"spotify"> {
     /**
      * @title Seed Tracks
-     * 
+     *
      * An array of seed track IDs for recommendations.
      */
     seedTracks: string[];
@@ -150,13 +148,13 @@ export namespace ISpotify {
 
   /**
    * @title Get Recommendations Output
-   * 
+   *
    * Output interface for retrieving recommended tracks.
    */
   export interface IGetRecommendationsOutput {
     /**
      * @title Recommended Tracks
-     * 
+     *
      * An array containing the ID, name, and artist information of recommended tracks.
      */
     tracks: Array<{ id: string; name: string; artist: string }>;
