c1a6587333c6f04a6ab8ad5c061269e26e5c7da3
kakasoo <kscodebase@gmail.com>
2024-10-16 15:45:04 +0900

    feat: controller 정의

--- src/controllers/connector/crunchbase/CrunchbaseController.ts
+++ src/controllers/connector/crunchbase/CrunchbaseController.ts
@@ -1,7 +1,20 @@
 import { Controller } from "@nestjs/common";
+import { ICrunchbase } from "@wrtn/connector-api/lib/structures/connector/crunchbase/ICrunchbase";
 import { CrunchbaseProvider } from "../../../providers/connector/crunchbase/CrunchbaseProvider";
 
 @Controller("connector/crunchbase")
 export class CrunchbaseController {
   constructor(private readonly crunchbaseProvider: CrunchbaseProvider) {}
+
+  async getOrganizationData(
+    input: ICrunchbase.IGetOrganizationDataInput,
+  ): Promise<ICrunchbase.CrunchbaseResponse> {
+    return this.crunchbaseProvider.getOrganizationData(input);
+  }
+
+  async autocomplete(
+    input: ICrunchbase.IAutocompleteInput,
+  ): Promise<ICrunchbase.IAutocompleteOutput> {
+    return this.crunchbaseProvider.autocomplete(input);
+  }
 }
