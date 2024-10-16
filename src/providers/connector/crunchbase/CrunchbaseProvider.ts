f4a66286424895e180d9530f5170dcfaee268e1c
kakasoo <kscodebase@gmail.com>
2024-10-16 14:13:37 +0900

    feat: healthCheck api

--- src/providers/connector/crunchbase/CrunchbaseProvider.ts
+++ src/providers/connector/crunchbase/CrunchbaseProvider.ts
@@ -1,4 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import axios from "axios";
+import { ConnectorGlobal } from "../../../ConnectorGlobal";
 
 @Injectable()
-export class CrunchbaseProvider {}
+export class CrunchbaseProvider {
+  async healthCheck() {
+    try {
+      const url = `https://crunchbase-api.p.rapidapi.com/v1/health-check`;
+      const res = await axios.get(url, {
+        headers: {
+          "x-rapidapi-host": "crunchbase-api.p.rapidapi.com",
+          "x-rapidapi-key": ConnectorGlobal.env.RAPIDAPI_KEY,
+        },
+      });
+
+      return res.data.status === "OK" ? true : false;
+    } catch (err) {
+      console.error(JSON.stringify(err));
+      throw err;
+    }
+  }
+}
