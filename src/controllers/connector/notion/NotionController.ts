c892af43a43917d0a3e0d9c7d305bb0de3df71e5
kakasoo <kscodebase@gmail.com>
2024-10-21 13:41:35 +0900

    fix: empty page 임을 명시

--- src/controllers/connector/notion/NotionController.ts
+++ src/controllers/connector/notion/NotionController.ts
@@ -714,7 +714,7 @@ export class NotionController {
    * Since Notion prioritizes accessible pages during authentication, creating pages must be sub-pages within the page, which means that there must be a parent page.
    * Because this feature only creates pages and does not create content, we recommend that you call additional content creation connectors if you want to create content.
    *
-   * @summary Create page
+   * @summary Create empty page
    * @param input Information needed to create the page
    * @returns Unique ID of the generated page
    */
