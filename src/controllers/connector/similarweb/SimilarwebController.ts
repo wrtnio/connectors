96c9d696b4c25b6de78dceed3e7037c79682eeee
kakasoo <kscodebase@gmail.com>
2024-11-12 16:28:19 +0900

    feat: 시밀러웹과 크런치베이스에 대한 아이콘 추가

--- src/controllers/connector/similarweb/SimilarwebController.ts
+++ src/controllers/connector/similarweb/SimilarwebController.ts
@@ -1,6 +1,7 @@
 import { TypedBody, TypedRoute } from "@nestia/core";
 import { Controller } from "@nestjs/common";
 import { ISimilarweb } from "@wrtn/connector-api/lib/structures/connector/similarweb/ISimilarweb";
+import { RouteIcon } from "@wrtnio/decorators";
 import { SimilarwebProvider } from "../../../providers/connector/similarweb/SimilarwebProvider";
 import { retry } from "../../../utils/retry";
 
@@ -19,6 +20,9 @@ export class SimilarwebController {
    * @param input - The input parameters containing the domain name to retrieve information for.
    * @returns A promise that resolves to the domain information output, including status and detailed data.
    */
+  @RouteIcon(
+    "https://ecosystem-connector.s3.ap-northeast-2.amazonaws.com/icon/similarweb_full.svg",
+  )
   @TypedRoute.Post("get-domain-info")
   async getDomainInfo(
     @TypedBody() input: ISimilarweb.IGetDomainInfoInput,
