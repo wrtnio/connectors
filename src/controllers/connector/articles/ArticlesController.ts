1d329d5826f1869563379f97d81138a5191a6d50
kakasoo <kscodebase@gmail.com>
2024-11-28 22:34:50 +0900

    feat: add `write article` connector to wrtn ecosystem database

--- src/controllers/connector/articles/ArticlesController.ts
+++ src/controllers/connector/articles/ArticlesController.ts
@@ -1,4 +1,4 @@
-import { TypedBody } from "@nestia/core";
+import core, { TypedBody } from "@nestia/core";
 import { Controller } from "@nestjs/common";
 import { IArticle } from "@wrtn/connector-api/lib/structures/connector/articles/IArticles";
 import { DocumentProvider } from "../../../providers/connector/article/DocumentProvider";
@@ -21,6 +21,22 @@ export class ArticlesController {
     return 1 as const;
   }
 
+  /**
+   * Write Article to User Database
+   *
+   * Posts are managed in a snapshot-based structure, enabling rollback at any time.
+   * When a post is created, the system automatically generates the first snapshot.
+   * The most recent snapshot reflects the current state of the post.
+   * By editing the post or exporting it to external services, you can track which version—i.e., which snapshot—was exported and synchronize it using foreign keys.
+   * This system is designed to efficiently handle any post-like documents, such as those from Google Docs, Notion, or Reddit.
+   * Using Markdown as the standard format for writing and versioning, it identifies changes through a diff algorithm whenever edits are made.
+   * This allows you to compare previous and current versions, track changes, and synchronize updates across services.
+   * As these APIs store data exclusively in the Wrtn Technologies Ecosystem Team's database without relying on external APIs,
+   * this setup serves as an ideal starting point for creating and managing documents efficiently with robust version control.
+   *
+   * @sumamry Write Article
+   */
+  @core.TypedRoute.Post()
   async write(@TypedBody() input: IArticle.ICreate): Promise<any> {
     return DocumentProvider.create(input);
   }
