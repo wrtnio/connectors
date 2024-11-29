13427fa92e7435ac14af4b3407e91d951a86730c
kakasoo <kscodebase@gmail.com>
2024-11-29 17:13:29 +0900

    feat: `IExternalUser` header type defined

---
+++ src/api/structures/common/IExternalUser.ts
@@ -0,0 +1,18 @@
+export namespace IExternalUser {
+  export interface ExternalUserIdentifier {
+    /**
+     * @title application
+     */
+    "x-wrtn-application": string;
+
+    /**
+     * @title uid
+     */
+    "x-wrtn-uid": string;
+
+    /**
+     * @title password
+     */
+    "x-wrtn-password": string;
+  }
+}
