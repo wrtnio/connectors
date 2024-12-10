46ae917ab49c6681e4255277970d9a7eb327ab0f
kakasoo <kscodebase@gmail.com>
2024-12-10 17:07:03 +0900

    feat: spreadsheet format transform function

--- src/providers/connector/swal/spreadsheet/SpreadsheetFormatProvider.ts
+++ src/providers/connector/swal/spreadsheet/SpreadsheetFormatProvider.ts
@@ -1,7 +1,24 @@
 import { Prisma } from "@prisma/client";
+import { ISpreadsheetFormat } from "@wrtn/connector-api/lib/structures/connector/swal/spreadsheet/ISpreadsheetFormat";
+import { StrictOmit } from "@wrtn/connector-api/lib/structures/types/strictOmit";
 
 export namespace SpreadsheetFormatProvider {
   export namespace summary {
+    export const transform = (
+      input: Prisma.spreadsheet_formatsGetPayload<
+        ReturnType<typeof summary.select>
+      >,
+    ): StrictOmit<ISpreadsheetFormat, "deleted_at" | "spreadsheet_id"> => {
+      return {
+        id: input.id,
+        background_color: input.background_color as `#${string}`,
+        font_name: input.font_name,
+        text_alignment: input.text_alignment,
+        font_size: input.font_size.toNumber(),
+        created_at: input.created_at.toISOString(),
+      };
+    };
+
     export const select = () => {
       return {
         select: {
