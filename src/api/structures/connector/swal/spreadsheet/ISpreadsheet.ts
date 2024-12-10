5b5a68c3af377b6c12a75e56a89b65b84c3a9f61
kakasoo <kscodebase@gmail.com>
2024-12-10 16:00:46 +0900

    feat: defined IRequest types

--- src/api/structures/connector/swal/spreadsheet/ISpreadsheet.ts
+++ src/api/structures/connector/swal/spreadsheet/ISpreadsheet.ts
@@ -1,9 +1,80 @@
 import { tags } from "typia";
+import { IPage } from "../../../common/IPage";
 import { StrictOmit } from "../../../types/strictOmit";
 import { ISpreadsheetCell } from "./ISpreadsheetCell";
 import { ISpreadsheetExport } from "./ISpreadsheetExport";
 
 export namespace ISpreadsheet {
+  export namespace IRequest {
+    /**
+     * If an attribute exists,
+     * it returns only the result of 'AND' calculations of all the attributes.
+     *
+     * @title Search Conditions
+     */
+    export interface ISearch {
+      /**
+       * Spreadsheet IDs, not Spreadsheet Snapshot ID
+       * It is Spreadsheet's ID, Not Spreadsheet Snapshot ID and {@link ISpreadsheetExport spreadsheet_exports} ID.
+       *
+       * @title Spreadsheet IDs
+       */
+      ids?: Array<ISpreadsheet["id"]>;
+
+      /**
+       * @title Last Snapshot
+       */
+      snapshot: {
+        /**
+         * This property is not a complete match, but a feature
+         * that allows you to search for a title that contains that character.
+         *
+         * @title Title of spreadsheet
+         */
+        title?: string;
+
+        /**
+         * This property is not a complete match, but a feature
+         * that allows you to search for a title that contains that character.
+         *
+         * @title Description of spreadsheet
+         */
+        description?: string;
+
+        /**
+         * It means the number of cells you wish to have at least.
+         * You can adjust this value when you want to look up a lot of written sheets.
+         *
+         * @title minimum count of cells
+         */
+        minimum_cell_count?: number & tags.Type<"uint64">;
+      };
+    }
+
+    export type SortableColumns =
+      | "created_at"
+      | "snapshot.created_at"
+      | "snapshot.title";
+  }
+
+  /**
+   * If you want to look up the list of sheets you created,
+   * you can use this to check.
+   *
+   * @title Request
+   */
+  export interface IRequest extends IPage.IRequest {
+    /**
+     * @title Search
+     */
+    search?: IRequest.ISearch;
+
+    /**
+     * @title Sort
+     */
+    sort?: IPage.Sort<IRequest.SortableColumns>;
+  }
+
   export interface ISummary
     extends StrictOmit<
       ISpreadsheet,
@@ -12,7 +83,7 @@ export namespace ISpreadsheet {
     /**
      * @title Total count of cells
      */
-    cell_count: number & tags.Type<"uint64">;
+    total_cell_count: number & tags.Type<"uint64">;
   }
 
   export interface ICreate {
