61063ff6181e9c9ea20adb4deea32e4d3d7d8415
kakasoo <kscodebase@gmail.com>
2024-12-10 16:00:55 +0900

    feat: add `search` method for search options of spreadsheet

--- src/providers/connector/swal/spreadsheet/SpreadsheetProvider.ts
+++ src/providers/connector/swal/spreadsheet/SpreadsheetProvider.ts
@@ -1,6 +1,8 @@
+import { Prisma } from "@prisma/client";
 import { IExternalUser } from "@wrtn/connector-api/lib/structures/common/IExternalUser";
 import { IPage } from "@wrtn/connector-api/lib/structures/common/IPage";
 import { ISpreadsheet } from "@wrtn/connector-api/lib/structures/connector/swal/spreadsheet/ISpreadsheet";
+import { ConnectorGlobal } from "../../../../ConnectorGlobal";
 
 export namespace SpreadsheetProvider {
   export namespace sync {}
@@ -30,5 +32,77 @@ export namespace SpreadsheetProvider {
     input: any,
   ): Promise<IPage<ISpreadsheet.ISummary>> => {
     return {} as any;
+  };
+
+  export const search = async (
+    external_user: IExternalUser,
+    input?: ISpreadsheet.IRequest.ISearch,
+  ) => {
+    const condition: Prisma.spreadsheetsWhereInput["AND"] = [
+      {
+        external_user_id: external_user.id,
+        password: external_user.password,
+        deleted_at: null,
+      },
+    ];
+
+    if (input?.ids !== undefined) {
+      condition.push({
+        id: {
+          in: input.ids,
+        },
+      });
+    }
+
+    if (input?.snapshot.description !== undefined) {
+      condition.push({
+        mv_last: {
+          snapshot: {
+            description: {
+              contains: input.snapshot.description,
+            },
+          },
+        },
+      });
+    }
+
+    if (input?.snapshot.minimum_cell_count !== undefined) {
+      const res = await ConnectorGlobal.prisma.spreadsheet_cells.groupBy({
+        _count: true,
+        where: {
+          spreadsheet: {
+            external_user_id: external_user.id,
+            password: external_user.password,
+            deleted_at: null,
+          },
+          mv_last: {
+            isNot: null,
+          },
+        },
+        by: ["spreadsheet_id"],
+      });
+
+      const spreadsheet_ids = res
+        .filter((el): boolean => el._count > input.snapshot.minimum_cell_count!)
+        .map((el): string => el.spreadsheet_id);
+
+      condition.push({
+        id: {
+          in: spreadsheet_ids,
+        },
+      });
+    }
+
+    if (input?.snapshot.title !== undefined) {
+      condition.push({
+        mv_last: {
+          snapshot: {
+            title: {
+              contains: input.snapshot.title,
+            },
+          },
+        },
+      });
+    }
   };
 }
