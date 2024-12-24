4930a288002eb688ed999548ace9e8c1258cc3da
kakasoo <kscodebase@gmail.com>
2024-12-24 10:57:49 +0900

    feat: google Calendar를 namespace로 변경

--- src/controllers/connector/google_calendar/GoogleCalendarModule.ts
+++ src/controllers/connector/google_calendar/GoogleCalendarModule.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
 
-import { GoogleCalendarProvider } from "../../../providers/connector/google_calendar/GoogleCalendarProvider";
 import { GoogleCalendarController } from "./GoogleCalendarController";
 
 @Module({
   controllers: [GoogleCalendarController],
-  providers: [GoogleCalendarProvider],
-  exports: [GoogleCalendarProvider],
 })
 export class GoogleCalendarModule {}
