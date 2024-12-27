0f067a9d3e04ddc54f4e0ed5a06675a993545993
Leo <seunghwa333@gmail.com>
2024-12-27 16:49:48 +0900

    fix: remove serpapi youtube search

--- src/controllers/connector/youtube_search/YoutubeSearchController.ts
+++ src/controllers/connector/youtube_search/YoutubeSearchController.ts
@@ -11,45 +11,45 @@ import { retry } from "../../../utils/retry";
 @Controller("connector/youtube-search")
 export class YoutubeSearchController {
   constructor(private readonly youtubeSearchProvider: YoutubeSearchProvider) {}
-  /**
-   * Get YouTube video search results
-   *
-   * The search results have the video title and link.
-   *
-   * If most users are going to use this feature, they probably want to watch the video, so it's better to provide a URL.
-   *
-   * In order to filter the period that the user wants, you should use the response field "published_date".
-   *
-   * For example, if the user wants to retrieve only this year's videos, you should exclude videos that were uploaded in a period that the user does not want, such as "1 year ago" or "2 years ago" with a published_date.
-   *
-   * It's great to use with the /transcript endpoint when summarizing videos, analyzing content, extracting keywords, etc.
-   *
-   * Extract the URL from the YouTube video information obtained from the execution result of the corresponding function and use it as the input of the /transcript endpoint.
-   *
-   * Based on the transcripts obtained from the execution result of the /transcript endpoint, perform tasks such as summarizing videos, analyzing content, and extracting keywords.
-   *
-   * Example Use Cases:
-   * Product Reviews: Extract product names, pros, cons, and recommendations from air purifier review videos.
-   * Tutorials: Create text-based tutorials or step-by-step guides from instructional videos.
-   *
-   * @summary YouTube video search
-   * @param input Conditions for YouTube video search
-   * @returns List of YouTube video search results
-   */
-  @Standalone()
-  @core.TypedRoute.Post()
-  @RouteIcon(
-    "https://ecosystem-connector.s3.ap-northeast-2.amazonaws.com/icon/fulls/Youtube_full.svg",
-  )
-  @ApiTags("Youtube")
-  async search(
-    @core.TypedBody() input: IYoutubeSearch.ISearchInput,
-  ): Promise<IYoutubeSearch.ISearchOutput[]> {
-    return retry(() => this.youtubeSearchProvider.search(input))();
-  }
+  // /**
+  //  * Get YouTube video search results
+  //  *
+  //  * The search results have the video title and link.
+  //  *
+  //  * If most users are going to use this feature, they probably want to watch the video, so it's better to provide a URL.
+  //  *
+  //  * In order to filter the period that the user wants, you should use the response field "published_date".
+  //  *
+  //  * For example, if the user wants to retrieve only this year's videos, you should exclude videos that were uploaded in a period that the user does not want, such as "1 year ago" or "2 years ago" with a published_date.
+  //  *
+  //  * It's great to use with the /transcript endpoint when summarizing videos, analyzing content, extracting keywords, etc.
+  //  *
+  //  * Extract the URL from the YouTube video information obtained from the execution result of the corresponding function and use it as the input of the /transcript endpoint.
+  //  *
+  //  * Based on the transcripts obtained from the execution result of the /transcript endpoint, perform tasks such as summarizing videos, analyzing content, and extracting keywords.
+  //  *
+  //  * Example Use Cases:
+  //  * Product Reviews: Extract product names, pros, cons, and recommendations from air purifier review videos.
+  //  * Tutorials: Create text-based tutorials or step-by-step guides from instructional videos.
+  //  *
+  //  * @summary YouTube video search
+  //  * @param input Conditions for YouTube video search
+  //  * @returns List of YouTube video search results
+  //  */
+  // @Standalone()
+  // @core.TypedRoute.Post()
+  // @RouteIcon(
+  //   "https://ecosystem-connector.s3.ap-northeast-2.amazonaws.com/icon/fulls/Youtube_full.svg",
+  // )
+  // @ApiTags("Youtube")
+  // async search(
+  //   @core.TypedBody() input: IYoutubeSearch.ISearchInput,
+  // ): Promise<IYoutubeSearch.ISearchOutput[]> {
+  //   return retry(() => this.youtubeSearchProvider.search(input))();
+  // }
 
   /**
-   * Get YouTube video search results (official)
+   * Get YouTube video search results
    *
    * This function use the official YouTube API to search for videos.
    *
@@ -77,7 +77,7 @@ export class YoutubeSearchController {
    * Product Reviews: Extract product names, pros, cons, and recommendations from air purifier review videos.
    * Tutorials: Create text-based tutorials or step-by-step guides from instructional videos.
    *
-   * @summary YouTube video search (official)
+   * @summary YouTube video search
    * @param input
    * @returns
    */
--- test/features/api/connector/youtube/test_api_connector_youtube_search.ts
+++ test/features/api/connector/youtube/test_api_connector_youtube_search.ts
@@ -1,19 +1,19 @@
-import typia from "typia";
+// import typia from "typia";
 
-import CApi from "@wrtn/connector-api/lib/index";
-import { IYoutubeSearch } from "@wrtn/connector-api/lib/structures/connector/youtube_search/IYoutubeSearch";
+// import CApi from "@wrtn/connector-api/lib/index";
+// import { IYoutubeSearch } from "@wrtn/connector-api/lib/structures/connector/youtube_search/IYoutubeSearch";
 
-export const test_api_connector_youtube_search = async (
-  connection: CApi.IConnection,
-): Promise<IYoutubeSearch.ISearchOutput[]> => {
-  const result = await CApi.functional.connector.youtube_search.search(
-    connection,
-    {
-      and_keywords: ["뤼튼", "AI"],
-      or_keywords: ["스튜디오", "생태계"],
-      not_keywords: ["정치", "폭력"],
-    },
-  );
-  typia.assert(result);
-  return result;
-};
+// export const test_api_connector_youtube_search = async (
+//   connection: CApi.IConnection,
+// ): Promise<IYoutubeSearch.ISearchOutput[]> => {
+//   const result = await CApi.functional.connector.youtube_search.search(
+//     connection,
+//     {
+//       and_keywords: ["뤼튼", "AI"],
+//       or_keywords: ["스튜디오", "생태계"],
+//       not_keywords: ["정치", "폭력"],
+//     },
+//   );
+//   typia.assert(result);
+//   return result;
+// };
